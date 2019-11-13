5f3256b6b0e8cc2549d69cf7a5c52e79756ab253
tar-aldev <frontdev.at@gmail.com>
2019-11-13 11:01:55 +0200

    font-sizes

--- src/pages/AddPost/AddPost.js
+++ src/pages/AddPost/AddPost.js
@@ -1,92 +1,87 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from 'react'
 import {
   Editor,
   EditorState,
   RichUtils,
   convertFromRaw,
-  convertToRaw,
-} from "draft-js";
-import "draft-js/dist/Draft.css";
-import { Button, Card, FormControl } from "react-bootstrap";
-import classes from "./AddPost.module.scss";
-import clsx from "clsx";
-import { EditorControls } from "./EditorControls/EditorControls";
-
-const fontSizes = Array.from(Array(50).keys()).filter(
-  value => value > 9 && value % 2 === 0
-);
-
-const fontsMap = fontSizes.reduce((acc, curr) => {
-  acc[curr] = { fontSize: `${curr}px` };
-  return acc;
-}, {});
-
-const rootStylesMap = {
-  ...fontsMap,
-};
+  convertToRaw
+} from 'draft-js'
+import 'draft-js/dist/Draft.css'
+import { Card } from 'react-bootstrap'
+import classes from './AddPost.module.scss'
+import clsx from 'clsx'
+import { EditorControls } from './EditorControls/EditorControls'
 
 export const AddPost = () => {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const editorRef = useRef({});
+  const [editorState, setEditorState] = useState(EditorState.createEmpty())
+  const editorRef = useRef({})
 
   useEffect(() => {
-    editorRef.current.focus();
-  }, []);
+    editorRef.current.focus()
+  }, [])
 
   const handleChange = editorState => {
-    setEditorState(editorState);
-  };
+    setEditorState(editorState)
+  }
 
   const handleKeyCommand = (command, newEditorState) => {
-    console.log("command", command);
-    const newState = RichUtils.handleKeyCommand(newEditorState, command);
+    console.log('command', command)
+    const newState = RichUtils.handleKeyCommand(newEditorState, command)
 
     if (newState) {
-      handleChange(newState);
-      return "handled";
+      handleChange(newState)
+      return 'handled'
     }
-    return "not-handled";
-  };
+    return 'not-handled'
+  }
 
   const convert = () => {
-    const raw = convertToRaw(editorState.getCurrentContent());
-    const unpacked = convertFromRaw(raw);
-    console.log("RAW", raw, "unpacked", unpacked);
-  };
+    const raw = convertToRaw(editorState.getCurrentContent())
+    const unpacked = convertFromRaw(raw)
+    console.log('RAW', raw, 'unpacked', unpacked)
+  }
 
   const blockStyleCustomizer = contentBlock => {
-    if (contentBlock.getType() === "blockquote") {
-      return classes.qoutedText;
+    console.log('block style customizer')
+    console.log('contentBlock.getType()', contentBlock.getType())
+    const blockType = contentBlock.getType()
+
+    switch (blockType) {
+      case 'blockquote':
+        return classes.qoutedText
+      case 'code-block':
+        return classes.codeBlock
+      case 'textLeft':
+      case 'textCenter':
+      case 'textRight':
+        return classes[blockType]
+      default:
+        return ''
     }
-    if (contentBlock.getType() === "code-block") {
-      return classes.codeBlock;
-    }
-  };
+  }
 
   return (
-    <div className="py-4">
+    <div className='py-4'>
       <h6>Write your own post</h6>
-      <Card style={{ height: "80%" }}>
-        <Card.Body className="p-0">
-          <div className={clsx(classes.editor, "p-2")}>
+      <Card style={{ height: '80%' }}>
+        <Card.Body className='p-0'>
+          <div className={clsx(classes.editor, 'p-2')}>
             <EditorControls
-              fontsMap={fontsMap}
               editorState={editorState}
               handleChange={handleChange}
               editorRef={editorRef}
             />
-            <hr className="my-1 bg-info" />
+            <hr className='my-1 bg-info' />
             <Editor
               editorState={editorState}
               handleKeyCommand={handleKeyCommand}
               onChange={handleChange}
               blockStyleFn={blockStyleCustomizer}
-              customStyleMap={rootStylesMap}
               ref={editorRef}
             />
           </div>
         </Card.Body>
       </Card>
     </div>
-  );
-};
+  )
+}
--- src/pages/AddPost/EditorControls/EditorControls.js
+++ src/pages/AddPost/EditorControls/EditorControls.js
@@ -1,149 +1,180 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
-import clsx from "clsx";
-import { RichUtils } from "draft-js";
+import React, { useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+import clsx from 'clsx'
+import { RichUtils } from 'draft-js'
 
-import classes from "./EditorControls.module.scss";
+import classes from './EditorControls.module.scss'
 
 const TEXT_CONTROLS_LIST = [
   {
-    value: "BOLD", // what is passed to RichUtils.toggleInlineStyle
-    icon: "fas fa-bold",
+    value: 'BOLD', // what is passed to RichUtils.toggleInlineStyle
+    icon: 'fas fa-bold'
   },
   {
-    value: "ITALIC",
-    icon: "fas fa-italic",
+    value: 'ITALIC',
+    icon: 'fas fa-italic'
   },
   {
-    value: "UNDERLINE",
-    icon: "fas fa-underline",
+    value: 'UNDERLINE',
+    icon: 'fas fa-underline'
   },
   {
-    value: "STRIKETHROUGH",
-    icon: "fas fa-strikethrough",
-  },
-];
+    value: 'STRIKETHROUGH',
+    icon: 'fas fa-strikethrough'
+  }
+]
 
 const BLOCK_TYPES_LIST = [
   {
-    value: "blockquote",
-    icon: "fas fa-quote-left",
+    value: 'textLeft',
+    icon: 'fas fa-align-left'
   },
   {
-    value: "code-block",
-    icon: "fas fa-code",
+    value: 'textCenter',
+    icon: 'fas fa-align-center'
   },
-];
+  {
+    value: 'textRight',
+    icon: 'fas fa-align-right'
+  },
+  {
+    value: 'blockquote',
+    icon: 'fas fa-quote-left'
+  },
+  {
+    value: 'code-block',
+    icon: 'fas fa-code'
+  }
+]
+
+const FONT_SIZES = [
+  {
+    value: 'unstyled',
+    label: 'Normal'
+  },
+  {
+    value: 'header-five',
+    label: 'Subtitle'
+  },
+  {
+    value: 'header-three',
+    label: 'Title'
+  }
+]
 
 export const EditorControls = ({
-  fontsMap,
+  getStyles,
   handleChange,
   editorState,
-  editorRef,
+  editorRef
 }) => {
-  const [menuShown, setMenuShown] = useState(false);
-  const [selectedFontSize, setSelectedFontSize] = useState(14);
+  const [menuShown, setMenuShown] = useState(false)
+  const selection = editorState.getSelection()
 
-  useEffect(() => {}, []);
-
-  const currentInlineStyles = editorState.getCurrentInlineStyle();
-
-  const toggleFontsMenu = e => {
-    e.preventDefault();
-    setMenuShown(!menuShown);
-  };
-
-  const preventDefault = (action, ...args) => e => {
-    e.preventDefault();
-    action(...args);
-  };
-
-  const toggleBlock = value => {
-    handleChange(RichUtils.toggleBlockType(editorState, value));
-  };
-
-  const toggleInlineStyle = value => {
-    handleChange(RichUtils.toggleInlineStyle(editorState, value));
-  };
-
-  const toggleFontSize = value => {
-    console.log(value, "font size", editorRef.current);
-    const newEditroState = RichUtils.toggleInlineStyle(
-      editorState,
-      value.toString()
-    );
-    setSelectedFontSize(value);
-    handleChange(newEditroState);
-    setMenuShown(false);
-  };
-
-  const selection = editorState.getSelection();
   const blockType = editorState
     .getCurrentContent()
     .getBlockForKey(selection.getStartKey())
-    .getType();
+    .getType()
 
-  console.log("currentInlineStyles", currentInlineStyles.toObject());
+  const fontSize = FONT_SIZES.find(fontSize => fontSize.value === blockType)
+  const currentInlineStyles = editorState.getCurrentInlineStyle()
+
+  const toggleFontsMenu = e => {
+    e.preventDefault()
+    setMenuShown(!menuShown)
+  }
+
+  const preventDefault = (action, ...args) => e => {
+    e.preventDefault()
+    if (action) {
+      action(...args)
+    }
+  }
+
+  const toggleBlock = value => {
+    handleChange(RichUtils.toggleBlockType(editorState, value))
+  }
+
+  const toggleInlineStyle = value => {
+    handleChange(RichUtils.toggleInlineStyle(editorState, value))
+  }
+
+  const toggleFontSize = value => {
+    console.log(value, 'font size', editorRef.current)
+    handleChange(RichUtils.toggleBlockType(editorState, value))
+    setMenuShown(false)
+  }
+
   /* ***BUG*** when toolbar button is clicked and editor is not focused styling is not applied */
   return (
     <div className={classes.controls}>
       {TEXT_CONTROLS_LIST.map(control => (
         <Button
           key={`${control.value}-${control.icon}`}
-          variant="outline-light"
-          size="sm"
+          variant='outline-light'
+          size='sm'
           onMouseDown={preventDefault(toggleInlineStyle, control.value)}
           className={clsx(
             classes.controlBtn,
             currentInlineStyles.has(control.value) && classes.highlight
           )}
         >
-          <i className={control.icon}></i>
+          <i className={control.icon} />
         </Button>
       ))}
 
-      <div className={clsx(classes.verticalDivider, "bg-white mx-1")} />
+      <div className={clsx(classes.verticalDivider, 'bg-white mx-1')} />
 
       {BLOCK_TYPES_LIST.map(blockTypeControl => (
         <Button
           key={`${blockTypeControl.value}-${blockTypeControl.icon}`}
-          variant="outline-light"
-          size="sm"
+          variant='outline-light'
+          size='sm'
           onMouseDown={preventDefault(toggleBlock, blockTypeControl.value)}
           className={clsx(
             classes.controlBtn,
             blockTypeControl.value === blockType && classes.highlight
           )}
         >
-          <i className={blockTypeControl.icon}></i>
+          <i className={blockTypeControl.icon} />
         </Button>
       ))}
 
-      <div className={classes.customSelectWrapper}>
+      <div
+        className={classes.customSelectWrapper}
+        onMouseLeave={preventDefault(setMenuShown, false)}
+      >
         <Button
-          variant="outline-light"
-          size="sm"
-          className={classes.controlBtn}
-          onMouseDown={toggleFontsMenu}
+          variant='outline-light'
+          size='sm'
+          className={clsx(
+            classes.controlBtn,
+            'd-flex w-100 justify-content-between'
+          )}
+          onMouseDown={preventDefault(null)}
+          onMouseEnter={toggleFontsMenu}
         >
-          {selectedFontSize}
+          <span className='d-block'>
+            {fontSize ? fontSize.label : `Normal`}
+          </span>
+          <i className='fas fa-sort-down' />
         </Button>
         {menuShown && (
           <div className={classes.selectMenu}>
-            {Object.keys(fontsMap).map(font => (
+            {FONT_SIZES.map(font => (
               <Button
-                key={font}
-                variant="outline-light"
-                size="sm"
+                key={font.value}
+                variant='outline-light'
+                size='sm'
                 className={classes.controlBtn}
-                onMouseDown={preventDefault(toggleFontSize, font)}
+                onMouseDown={preventDefault(toggleFontSize, font.value)}
               >
-                {font}
+                {font.label}
               </Button>
             ))}
           </div>
         )}
       </div>
     </div>
-  );
-};
+  )
+}

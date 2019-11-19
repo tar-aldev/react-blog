a852e31431fceec41123af17b9655c2bd5d297be
tar-aldev <frontdev.at@gmail.com>
2019-11-19 11:47:08 +0200

    fixed text alignment

--- src/pages/AddPost/plugins/blockNodes.plugin.js
+++ src/pages/AddPost/plugins/blockNodes.plugin.js
@@ -1,65 +1,65 @@
-import React from 'react'
-import BlockQuoteNode from '../Nodes/BlockQuoteNode/BlockQuoteNode'
+import React from "react";
+import BlockQuoteNode from "../Nodes/BlockQuoteNode/BlockQuoteNode";
 
 const blockRender = options => {
-  const { component, type } = options
+  const { component, type } = options;
 
   return {
     renderBlock: (props, editor, next) => {
       if (props.node.type === type) {
-        return component(props)
+        return component(props);
       }
-      return next()
-    }
-  }
-}
+      return next();
+    },
+  };
+};
 
 const textLeft = blockRender({
-  type: 'text-left',
+  type: "text-left",
   component: props => (
-    <div style={{ textAlign: 'left' }} {...props.attributes}>
+    <div style={{ textAlign: "left" }} {...props.attributes}>
       {props.children}
     </div>
-  )
-})
+  ),
+});
 
 const textCenter = blockRender({
-  type: 'text-center',
+  type: "text-center",
   component: props => (
-    <div style={{ textAlign: 'left' }} {...props.attributes}>
+    <div style={{ textAlign: "center" }} {...props.attributes}>
       {props.children}
     </div>
-  )
-})
+  ),
+});
 
 const textRight = blockRender({
-  type: 'text-left',
+  type: "text-right",
   component: props => (
-    <div style={{ textAlign: 'left' }} {...props.attributes}>
+    <div style={{ textAlign: "right" }} {...props.attributes}>
       {props.children}
     </div>
-  )
-})
+  ),
+});
 
 const blockQuote = blockRender({
-  type: 'blockquote',
-  component: props => <BlockQuoteNode {...props} />
-})
+  type: "blockquote",
+  component: props => <BlockQuoteNode {...props} />,
+});
 
 const orderedList = blockRender({
-  type: 'ordered-list',
-  component: props => <ol {...props.attributes}>{props.children}</ol>
-})
+  type: "ordered-list",
+  component: props => <ol {...props.attributes}>{props.children}</ol>,
+});
 
 const unorderedList = blockRender({
-  type: 'unordered-list',
-  component: props => <ul {...props.attributes}>{props.children}</ul>
-})
+  type: "unordered-list",
+  component: props => <ul {...props.attributes}>{props.children}</ul>,
+});
 
 const listItem = blockRender({
-  type: 'list-item',
-  component: props => <li {...props.attributes}>{props.children}</li>
-})
+  type: "list-item",
+  component: props => <li {...props.attributes}>{props.children}</li>,
+});
 
 export default [
   textLeft,
@@ -68,5 +68,5 @@ export default [
   blockQuote,
   orderedList,
   unorderedList,
-  listItem
-]
+  listItem,
+];
